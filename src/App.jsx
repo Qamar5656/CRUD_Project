af108df469a41bab04e4fc1586c0923518a64f45
Qamar5656 <qamar4157@gmail.com>
2025-11-19 12:38:25 +0500

    updating main file to load parent component to display users

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import SimpleForm from "./components/SimpleForm.jsx";
-import UsersList from "./components/UserList.jsx";
-
+// import SimpleForm from "./components/SimpleForm.jsx";
+// import UsersList from "./components/UserList.jsx";
+import UsersPage from "./components/ParentUsers.jsx";
 function App() {
   return (
     <>
-      <SimpleForm />
-      <UsersList />
+      <UsersPage />
+      {/* <SimpleForm />
+      <UsersList /> */}
     </>
   );
 }
