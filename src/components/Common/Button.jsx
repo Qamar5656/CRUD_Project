811619218d4d6f82ec405c51a11327e42204acb5
Qamar5656 <qamar4157@gmail.com>
2025-11-24 14:54:12 +0500

    adding reusable inputfield , button and model components

---
+++ src/components/Common/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({ caption }) => {
+  return (
+    <>
+      <button className="cursor-pointer bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 rounded-lg font-semibold hover:scale-105 transform transition">
+        {caption}
+      </button>
+    </>
+  );
+};
+
+export default Button;
