0e9dfed17987ef390892fb9b8025e657be8cf0ac
Qamar5656 <qamar4157@gmail.com>
2025-11-25 18:18:08 +0500

    forget and reset password functionality

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ caption }) => {
   return (
     <>
-      <button className="cursor-pointer bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 rounded-lg font-semibold hover:scale-105 transform transition">
+      <button className="cursor-pointer bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 px-3 rounded-lg font-semibold hover:scale-105 transform transition">
         {caption}
       </button>
     </>
