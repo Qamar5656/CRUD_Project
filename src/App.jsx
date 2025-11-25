6e68d99bde438f9611e78e425d1e650fd1a05780
Qamar5656 <qamar4157@gmail.com>
2025-11-25 18:17:48 +0500

    updating routes and api calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,78 @@
 import React from "react";
-import "./App.css";
-import UsersPage from "./components/UsersPage/UsersPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./components/SignIn/SIgnIn";
 import SignUp from "./components/SignUp/SIgnUp";
-function App() {
+import Dashboard from "./components/UsersPage/Dashboard";
+import PublicRoute from "./routes/PublicRoute.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import VerifyOtp from "./components/OtpForm/VerifyOtp.jsx";
+import ForgetPassword from "./components/forgetPassword/ForgetPassword.jsx";
+import ResetPassword from "./components/resetPassword/ResetPassword.jsx";
+
+const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute>
+              <SignIn />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <SignUp />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/verify-otp"
+          element={
+            <PublicRoute>
+              <VerifyOtp />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/forget-password"
+          element={
+            <PublicRoute>
+              <ForgetPassword />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/reset-password"
+          element={
+            <PublicRoute>
+              <ResetPassword />
+            </PublicRoute>
+          }
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Redirect any unknown route to SignIn */}
+        <Route path="*" element={<Navigate to="/signin" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
