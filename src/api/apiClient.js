36f4adec3147ecafe6bd8b3b995aa4dff066204c
Qamar5656 <qamar4157@gmail.com>
2025-11-26 18:44:14 +0500

    uploading gitignore file

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  headers: { "Content-Type": "application/json" },
+  // headers: { "Content-Type": "application/json" },
+  // headers: { "Content-Type": "multipart/form-data" },
 });
 
 export default apiClient;
