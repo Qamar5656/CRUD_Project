11070ec7b97ff1cc4ecaa82d3f2b603efcb9044e
Qamar5656 <qamar4157@gmail.com>
2025-12-03 10:07:59 +0500

    updating button component

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = ({ caption }) => {
+const Button = ({ caption, className }) => {
+  const baseClass =
+    "cursor-pointer bg-gray-800 text-white py-3 px-5 rounded-lg font-semibold hover:scale-105 transform transition";
   return (
     <>
-      <button className="cursor-pointer bg-gradient-to-r from-blue-500 to-purple-500 text-white py-3 px-3 rounded-lg font-semibold hover:scale-105 transform transition">
-        {caption}
-      </button>
+      <button className={`${baseClass} + ${className}`}>{caption}</button>
     </>
   );
 };
